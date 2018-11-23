774bc6fe5f7ac7d2121dfcb9ef90e1426a8826e6
Diego Ramos <ddramoss@unal.edu.co>
2018-11-22 23:29:59 -0500

    Ventanas externas funcionando

--- proyectos/diegodiego13/assets/js/script.js
+++ proyectos/diegodiego13/assets/js/script.js
@@ -1,70 +1,76 @@
-/*Para programar el botón que muestre el menú lateral*/
-var btnMenuOpen = document.getElementById("btn-menu-open");
-var menuLateral = document.getElementById("menu-lateral");
-var btnMenuClose = document.getElementById("btn-menu-close");
-var btnNavToggle = document.getElementById("btn-nav-toggle");
-var navLinks = document.getElementById("nav-links");
+var btnMenuOpen= document.getElementById("btn-menu-open");
+var menuLateral= document.getElementById("menu-lateral");
+var btnMenuClose= document.getElementById("btn-menu-close");
+var btnNavToggle= document.getElementById("btn-nav-toggle");
+var navLinks= document.getElementById("nav-links");
 
 //Para agregar una función a un elemento del documento se utiliza el addEventListener
 btnMenuOpen.addEventListener("click",mostrarMenuLateral);
-btnMenuClose.addEventListener("click",ocultarMenuLateral);
-btnNavToggle.addEventListener("click",toggleNavLinks);
+btnMenuClose.addEventListener("click", ocultarMenuLateral);
+btnNavToggle.addEventListener("click", toggleNaveLinks);
 window.addEventListener("resize",arreglarNavLinks);
 
-//Función para mostrar el menú lateral
-function mostrarMenuLateral() {
+function mostrarMenuLateral(){
+    //Agregamos una clase a un elemento
     menuLateral.classList.add("mostrar");
 }
 
-//Función para ocultar el menú lateral
-function ocultarMenuLateral() {
-    menuLateral.classList.remove("mostrar");
-} 
 
-//Función para mostrar el nav
-function toggleNavLinks() {
+function ocultarMenuLateral(){
+    //Quitamos la clase del elemento
+    menuLateral.classList.remove("mostrar")
+}
+
+function toggleNaveLinks(){
     navLinks.classList.toggle("mostrar");
+
 }
 
 function arreglarNavLinks() {
-    if(window.innerWidth >= 900) {
+    if(window.innerWidth >= 900){
         navLinks.classList.remove("mostrar");
     }
 }
 function cargarDatos() {
     var datos = [
         {url: "//unal.edu.co", nombre: "UNAL", instruccion: "Instrucción UNAL"},
-        {url: "//css-trick.com", nombre: "CSS Tricks", instruccion: "Instrucción CSS"},
+        {url: "//css-tricks.com", nombre: "CSS Tricks", instruccion: "Instrucción CSS"},
         {url: "//gaia.manizales.unal.edu.co", nombre: "GAIA", instruccion: "Instrucción GAIA"},
-        {url: "assets/uploads/actividades/actividad-normal/index.html", nombre: "Determinar operación", instruccion: "Indique el signo que se requiere para llegar a ese resultado"},
-        {url: "assets/uploads/actividades/actividad-canvas/index.html", nombre: "Otra actividad", instruccion: "Indique el signo que se requiere para llegar a ese resultado"}
+        {url: "assets/uploads/actividades/actividad-normal/index.html", nombre: "Determinar operación", instruccion: "Operaciones simples"},
+        {url: "assets/uploads/actividades/actividad-canvas/index.html", nombre: "Arrastrar (Canvas)", instruccion: "Arrastrar implementando CANVAS"}
     ];
     return datos;
 }
 
-function generarLinks(){
-    var menuLinks=document.getElementById("menu-links");
-    menuLinks.innerHTML= "";
-    var links= cargarDatos();
-    if(links.length>0){
-        //Cargaron los datos
-        for (var i = 0; i < links.length; i++) {
-            var texto=document.createTextNode(links[i].nombre);
-            var link=document.createElement("a");
-            //Le  vamos a poner un atributo
-            link.href=links[i].url;
-            link.target="main-iframe";
+function generarLinks() {
+    var menuLinks = document.getElementById("menu-links");
+    var divInstruc = document.getElementById("TxtInstruccion");
+    menuLinks.innerHTML = "";
+
+    var links = cargarDatos();
+    console.table(links);
+    if(links.length > 0) {
+        //Llegaron datos
+        for(var i = 0; i < links.length; i++) {
+            var texto = document.createTextNode(links[i].nombre);
+            // var instru = document.createTextNode(links[i].instruccion);
+            // divInstruc.appendChild(instru);
+            var link = document.createElement("a");
+            link.href = links[i].url;
+            link.target = "name-iframe";
             link.appendChild(texto);
-            var itemLista=document.createElement("li");
+
+            var itemLista = document.createElement("li");
             itemLista.appendChild(link);
+
             menuLinks.appendChild(itemLista);
         }
-    }
-    else{
-        //No llegaron los datows
-        var texto=document.createTextNode("No se ha encontrado ninguna actividad");
-        var itemLista=document.createElement("li");
+    }else{
+        var texto = document.createTextNode("No se ha encontrado ninguna actividad");
+        var itemLista = document.createElement("li");
         itemLista.appendChild(texto);
+        menuLinks.appendChild(itemLista);
     }
 }
+
 generarLinks();
